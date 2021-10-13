0f21145586195c1fc0be5fd6450b49981c0b1e48
Michael Rudolph <michaelrudolph@posteo.net>
2021-10-13 10:31:44 +0200

    Add documentation to the methods

--- src/version.comparator.ts
+++ src/version.comparator.ts
@@ -1,3 +1,13 @@
+/**
+ * Compares two version strings.
+ *
+ * Returns
+ * -1 if version < otherVersion
+ * 1 if version > otherVersion
+ * 0 if version == otherVersion
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function compare(version: string, otherVersion: string): number {
   // Split the version into its parts, e.g. '5.06' -> ['5', '06'],
   const v1 = version.split('.');
@@ -12,26 +22,56 @@ export function compare(version: string, otherVersion: string): number {
   return v1.length === v2.length ? 0 : v1.length < v2.length ? -1 : 1;
 }
 
+/**
+ * Checks if version <= otherVersion.
+ *
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function lte(version: string, otherVersion: string): boolean {
   const comp = compare(version, otherVersion);
   return comp !== 1;
 }
 
+/**
+ * Checks if version >= otherVersion.
+ *
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function gte(version: string, otherVersion: string): boolean {
   const comp = compare(version, otherVersion);
   return comp !== -1;
 }
 
+/**
+ * Checks if version > otherVersion.
+ *
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function gt(version: string, otherVersion: string): boolean {
   const comp = compare(version, otherVersion);
   return comp === 1;
 }
 
+/**
+ * Checks if version < otherVersion.
+ *
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function lt(version: string, otherVersion: string): boolean {
   const comp = compare(version, otherVersion);
   return comp === -1;
 }
 
+/**
+ * Checks if version == otherVersion.
+ *
+ * @param version A version string (e.g. 5.4.1 or 5.04)
+ * @param otherVersion A version string (e.g. 5.4.1 or 5.04)
+ */
 export function eq(version: string, otherVersion: string): boolean {
   const comp = compare(version, otherVersion);
   return comp === 0;
