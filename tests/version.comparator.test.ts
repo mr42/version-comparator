b6ee33d683c3b4af777924e10b6e39288a24f9ca
Michael Rudolph <michaelrudolph@posteo.net>
2021-10-13 10:13:00 +0200

    Add test for lte method

--- tests/version.comparator.test.ts
+++ tests/version.comparator.test.ts
@@ -1,35 +1,19 @@
-import { compare } from '../src/version.comparator';
 import { expect } from 'chai';
+import { lte } from '../src/version.comparator';
 
-describe('version comparator', (): void => {
-  it('comparison returns 0 if v1 == v2', (): void => {
-    let actual: number = compare('5.06', '5.06');
-    expect(actual).is.equal(0);
-    actual = compare('5.6', '5.06');
-    expect(actual).is.equal(0);
+describe('version comparator lte', (): void => {
+  it('returns true if v1 == v2', (): void => {
+    const actual: number = lte('5.06', '5.06');
+    expect(actual).true;
   });
 
-  it('comparison returns 1 if v1 > v2', (): void => {
-    let actual: number = compare('5.06', '5.04');
-    expect(actual).is.equal(1);
-    actual = compare('5.6', '5.04');
-    expect(actual).is.equal(1);
+  it('returns true if v1 < v2', (): void => {
+    const actual: number = lte('5.04', '5.06');
+    expect(actual).true;
   });
 
-  it('comparison returns -1 if v1 < v2', (): void => {
-    let actual: number = compare('5.04', '5.06');
-    expect(actual).is.equal(-1);
-    actual = compare('5.4', '5.06');
-    expect(actual).is.equal(-1);
-  });
-
-  it('comparison returns 1 if v1.length > v2.length ', (): void => {
-    const actual: number = compare('5.06.1', '5.06');
-    expect(actual).is.equal(1);
-  });
-
-  it('comparison returns -1 if v1.length < v2.length ', (): void => {
-    const actual: number = compare('5.06', '5.06.1');
-    expect(actual).is.equal(-1);
+  it('returns false if v1 > v2', (): void => {
+    const actual: number = lte('5.06', '5.04');
+    expect(actual).false;
   });
 });
